9f23eda84edc0d1c7f365e612fecc179579f3835
tkj <kahjun.teh@thetasp.com>
2024-12-17 16:30:09 +0800

    Provide token and use the page

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { X_TOKEN } from 'lib-a/service-x';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+
+    { provide: X_TOKEN, useValue: 'SOME_VALUE' },
+    provideRouter(routes),
+  ]
 };
--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ComponentY } from 'lib-a/component-y';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'pagey', component: ComponentY },
+];
